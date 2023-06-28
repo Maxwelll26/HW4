017c8953d55a92e0523cc168f6d2f4317e889e7d
keren_hovav_1 <keren.hovav@campus.technion.ac.il>
2023-06-28 13:32:19 +0300

    part A works good with java docs

--- src/LevelLargestSum.java
+++ src/LevelLargestSum.java
@@ -1,5 +1,23 @@
 import java.util.ArrayDeque;
+
+/**
+ * The LevelLargestSum class provides a method to find the level with the largest sum in a binary tree.
+ */
 public class LevelLargestSum {
+
+    /**
+     * Finds the level with the largest sum in a binary tree.
+     * If the tree is empty (root is null), it returns -1.
+     * queue - to store the nodes during the traversal. (an array that represents queue).
+     * the code processes each level of the binary tree until the queue is empty.
+     * It retrieves the first node from the front of the queue using the poll() method.
+     * It adds the value of the current node to levelSum.
+     * If the node has a left child, it adds the left child to the queue.
+     * If the node has a right child, it adds the right child to the queue.
+     * After processing each level, it compares the levelSum with the maxLevelSum
+     * @param root The root node of the binary tree.
+     * @return The level with the largest sum. If the tree is empty (root is null), returns -1.
+     */
     public static int getLevelWithLargestSum(BinNode<Integer> root) {
         if (root == null)
             return -1;
@@ -8,6 +26,7 @@ public class LevelLargestSum {
         int maxLevelSum = Integer.MIN_VALUE;
         int levelWithMaxSum = 0;
         int currentLevel = 0;
+
         while (!queue.isEmpty()) {
             int levelSize = queue.size();
             int levelSum = 0;
--- src/PathFromRoot.java
+++ src/PathFromRoot.java
@@ -1,5 +1,22 @@
+/**
+ * The PathFromRoot class provides a method to check if a path exists in a binary tree.
+ * The code uses recursion to traverse the binary tree and check if the path exists.
+ */
 public class PathFromRoot {
+
+    /**
+     * Checks if a given path exists in a binary tree.
+     * If the given path string is empty  it means we have reached the end of the path
+     * and the method returns true - base case.
+     * If the root node is not null and the data stored in the root node is equal to the first character
+     * of the path string - It creates a new string by excluding the first character from the original path string.
+     * calls recursively for the left child or the right child with the remaining str, until the sty is empty or false.
+     * @param root The root node of the binary tree.
+     * @param str  The path string to be checked.
+     * @return True if the path exists, false otherwise.
+     */
     public static boolean doesPathExist(BinNode<Character> root, String str) {
+
         if (str.isEmpty())
             return true;
         if (root != null && root.getData() == str.charAt(0)) {
